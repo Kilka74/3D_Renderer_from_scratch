d04937f85cea121d2fdf6ef2e5ccfa787b96594f
Viktar Makouski <mcviktor@tut.by>
2022-05-23 23:04:18 +0300

    remove trash

--- tmp.py
+++
@@ -1,7 +0,0 @@
-lines = []
-with open("source/Application.cpp", "r") as file:
-    lines = list(file.readlines())
-with open("source/Application.cpp", "w") as file:
-    for line in lines:
-        line = line[4:]
-        file.write(line)
