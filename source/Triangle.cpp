f5e25e6e3a7c806ce97a2ec7994a9cad276bacec
Viktar Makouski <mcviktor@tut.by>
2022-05-24 21:08:50 +0300

    please 4

--- source/Triangle.cpp
+++ source/Triangle.cpp
@@ -2,55 +2,37 @@
 
 
 void Triangle::MoveBack() {
-    //coordinates->x -= 1;
    auto tmp = coordinates->array;
    tmp[0] -= 1;
-   tmp[3] -= 1;
-   tmp[6] -= 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
 
 void Triangle::MoveForward() {
-   // coordinates->x += 1;
    auto tmp = coordinates->array;
    tmp[0] += 1;
-   tmp[3] += 1;
-   tmp[6] += 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
 
 void Triangle::MoveLeft() {
-    //coordinates->y += 1;
    auto tmp = coordinates->array;
    tmp[1] += 1;
-   tmp[4] += 1;
-   tmp[7] += 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
 
 void Triangle::MoveRight() {
-   // coordinates->y -= 1;
    auto tmp = coordinates->array;
    tmp[1] -= 1;
-   tmp[4] -= 1;
-   tmp[7] -= 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
 
 void Triangle::MoveUp() {
-   // coordinates->z += 1;
    auto tmp = coordinates->array;
    tmp[2] -= 1;
-   tmp[5] -= 1;
-   tmp[8] -= 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
 
 void Triangle::MoveDown() {
-   // coordinates->z -= 1;
    auto tmp = coordinates->array;
    tmp[2] += 1;
-   tmp[5] += 1;
-   tmp[8] += 1;
    *coordinates = sf::Glsl::Mat3(tmp);
 }
